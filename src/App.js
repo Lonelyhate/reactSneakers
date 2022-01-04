b006dce94e90fdc46a433bae624dc3f65c011783
Nikita Fedorov <nikitafedorov666@icloud.com>
2022-01-05 02:53:07 +0300

    part 2.5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,45 @@
 function App() {
   return (
     <div className="wrapper">
+        <div style={{display: 'none'}} className="overlay">
+            <div className="drawer">
+                <h2>Корзина <img src="img/btn-remove.svg" alt="remove" /></h2>
+
+                <div className="items">
+                    <div className="cartItem">
+                        <div 
+                            style={{backgroundImage: 'url(img/sneakers/1.jpg)'}} 
+                            className="cartItemImg">
+                        </div>
+                        <div>
+                            <p>Мужские Кроссовки Nike Air Max 270</p>
+                            <b>12 999 руб.</b>
+                        </div>
+                        <button><img src="img/btn-remove.svg" alt="remove" /></button>
+                    </div>
+                </div>
+
+                <div className="cartTotalBlock">
+                    <ul>
+                        <li>
+                            <span>Итого: </span>
+                            <div></div>
+                            <b>21 498 руб.</b>
+                        </li>
+                        <li>
+                            <span>Налог 5%: </span>
+                            <div></div>
+                            <b>1074 руб. </b>
+                        </li>
+                    </ul>
+                    <button className="greenButton">
+                        Оформить заказ
+                        <img src="img/arrow-right.svg" alt="arrow"/>
+                    </button>
+                </div>
+            </div>
+        </div>
+
         <header>
             <div className="headerLeft">
                 <img width={40} height={40} src="img/logo.png" />
@@ -21,10 +60,19 @@ function App() {
             </ul>
         </header>
         <div className="content">
-            <h1>Все кроссовки</h1>
+            <div className="sheakersHeader">
+                <h1>Все кроссовки</h1>
+                <div className="search-block">
+                    <img src="img/search.svg" alt="Search"></img>
+                    <input placeholder="Поиск..."></input>
+                </div>
+            </div>
             <div className="sneakers">
                 <div className="card">
-                    <img width={133} height={112} src="img/sneakers/1.jpg" alt=""></img>
+                    <div className="favorite">
+                        <img src="img/heart-unliked.svg"></img>
+                    </div>
+                    <img width={133} height={112} src="img/sneakers/1.jpg" alt="Кроссовки"></img>
                     <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
                     <div className="cardBottom">
                         <div>
@@ -51,6 +99,19 @@ function App() {
                 </div>
                 <div className="card">
                     <img width={133} height={112} src="img/sneakers/3.jpg" alt=""></img>
+                    <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+                    <div className="cardBottom">
+                        <div>
+                            <span>Цена:</span>
+                            <b>12 999 руб.</b>
+                        </div>
+                        <button className="button">
+                            <img width={11} height={11} src="img/plus.svg"></img>
+                        </button>
+                    </div>
+                </div>
+                <div className="card">
+                    <img width={133} height={112} src="img/sneakers/4.jpg" alt=""></img>
                     <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
                     <div className="cardBottom">
                         <div>
